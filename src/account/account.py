ce4ae017e9b2fdecdf61a1999630eacf755ef387
Lewis Moore <lewismooredev@gmail.com>
2023-02-16 10:56:45 +0000

    Add check_balance function

--- src/account/account.py
+++ src/account/account.py
@@ -2,3 +2,6 @@ class Account:
     def __init__(self, name, balance = 0):
         self.name = name
         self.balance = balance
+    
+    def check_balance(self):
+        return self.balance
--- src/account/account_test.py
+++ src/account/account_test.py
@@ -10,3 +10,6 @@ class AccountTest(TestCase):
 
     def test_accountBalanceIsZeroOnCreation(self):
         assert self.newAccount.balance == 0
+
+    def test_checkBalanceReturnsBalance(self):
+        assert self.newAccount.check_balance() == 0
