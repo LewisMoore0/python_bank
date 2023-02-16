11ea4dbe3b29e8df3c65eef8a031f0b54ef0aea3
Lewis Moore <lewismooredev@gmail.com>
2023-02-16 11:50:15 +0000

    deposit reduces persons cash and increases accounts balance

--- src/person/person.py
+++ src/person/person.py
@@ -2,3 +2,10 @@ class Person:
     def __init__(self, name, cash = 0):
         self.name = name
         self.cash = cash
+
+    def deposit(self, number, account):
+        self.cash -= number
+        account.deposit(number)
+   
+
+
--- src/person/person_test.py
+++ src/person/person_test.py
@@ -1,12 +1,26 @@
 from unittest import TestCase
 from person.person import Person
+from account.account import Account
 
 class PersonTest(TestCase):
 
     person = Person('Lewis')
+    account = Account(person)
 
     def test_person(self):
         assert self.person.name == 'Lewis'
 
     def test_personHasCashAttribute0asDefault(self):
         assert self.person.cash == 0
+
+    def test_personDepositTakesAwayFromCash(self):
+        testPerson = Person('Lewis', 150)
+        testPerson.deposit(100, self.account) 
+        assert testPerson.cash == 50
+    
+    def test_personDepositIncreasesTargetAccountBalance(self):
+        testPerson = Person('Lewis', 200)
+        testAccount = Account(testPerson)
+
+        testPerson.deposit(100, testAccount)
+        assert testAccount.balance == 100
